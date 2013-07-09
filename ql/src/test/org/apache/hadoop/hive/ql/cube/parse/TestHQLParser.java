ef6b54c0213b1e7f9f16abb515fc6e56f4467d51
jaideep.dhok <jaideep.dhok@inmobi.com>
2013-07-09 11:08:34 +0530

    Test to verify stirng literal case is preserved

--- ql/src/test/org/apache/hadoop/hive/ql/cube/parse/TestHQLParser.java
+++ ql/src/test/org/apache/hadoop/hive/ql/cube/parse/TestHQLParser.java
@@ -1,7 +1,6 @@
 package org.apache.hadoop.hive.ql.cube.parse;
 
-import static org.apache.hadoop.hive.ql.parse.HiveParser.TOK_GROUPBY;
-import static org.apache.hadoop.hive.ql.parse.HiveParser.TOK_INSERT;
+import static org.apache.hadoop.hive.ql.parse.HiveParser.*;
 import junit.framework.Assert;
 
 import org.apache.hadoop.hive.ql.parse.ASTNode;
@@ -22,7 +21,25 @@ public class TestHQLParser {
     ASTNode orderby = HQLParser.findNodeByPath(node, TOK_INSERT, HiveParser.TOK_ORDERBY);
     String expectedOrderBy = "a , g( b ), ( e  /  100 )";
     Assert.assertEquals(expectedOrderBy, HQLParser.getString(orderby).trim());
+  }
+  
+  
+  @Test
+  public void testLiteralCaseIsPreserved() throws Exception {
+    String literalQuery = "SELECT 'abc' AS col1, 'DEF' AS col2 FROM foo where col3='GHI' "
+        + "AND col4 = 'JKLmno'";
 
+    ASTNode tree = HQLParser.parseHQL(literalQuery);
+
+    ASTNode select = HQLParser.findNodeByPath(tree, TOK_INSERT, TOK_SELECT);
+    String selectStr = HQLParser.getString(select).trim();
+    String expectedSelect = "'abc'  col1 ,  'DEF'  col2";
+    Assert.assertEquals(expectedSelect, selectStr);
+
+    ASTNode where = HQLParser.findNodeByPath(tree, TOK_INSERT, TOK_WHERE);
+    String whereStr = HQLParser.getString(where).trim();
+    String expectedWhere = "(( col3  =  'GHI' ) and ( col4  =  'JKLmno' ))";
+    Assert.assertEquals(expectedWhere, whereStr);
   }
 
 }
