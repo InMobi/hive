2faf5761191fa3103a0d779fde584d494ed75bf5
Amareshwari Sriramadasu <amareshwari.sr@inmobi.com>
2014-01-24 15:12:16 +0530

    Add encoding and decoding for query string config

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -36,6 +36,7 @@ import java.util.regex.Pattern;
 
 import javax.security.auth.login.LoginException;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -1283,6 +1284,20 @@ public class HiveConf extends Configuration {
     } else {
       return Integer.parseInt(m.group(1));
     }
+  }
+
+  public void setStringEncoded(ConfVars var, String valueStr) {
+    if (valueStr != null) {
+      setVar(var, new String(Base64.encodeBase64(valueStr.getBytes())));
+    }
+  }
+
+  public String getStringDecoded(ConfVars var) {
+    String encodedStr = getVar(var);
+    if (encodedStr != null) {
+      return new String(Base64.decodeBase64(encodedStr.getBytes()));
+    }
+    return null;
   }
 
 }
