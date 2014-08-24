62ca6c1ce957a7bb409f8f87152f22c8a236a2ba
suma.shivaprasad <suma.shivaprasad@inmobi.com>
2014-08-24 17:06:13 +0530

    Removed unused code and fixed formatting

--- serde/src/java/org/apache/hadoop/hive/serde2/objectinspector/ThriftUnionObjectInspector.java
+++ serde/src/java/org/apache/hadoop/hive/serde2/objectinspector/ThriftUnionObjectInspector.java
@@ -37,8 +37,6 @@ import java.util.Map;
 public class ThriftUnionObjectInspector extends ReflectionStructObjectInspector implements UnionObjectInspector {
 
   private static final String FIELD_METADATA_MAP = "metaDataMap";
-  private static final String UNION_VALUE_FIELD = "value_";
-  private static final String UNION_TAG_FIELD = "setField_";
   private  List<ObjectInspector> ois;
 
   @Override
@@ -69,43 +67,6 @@ public class ThriftUnionObjectInspector extends ReflectionStructObjectInspector 
   }
 
   /**
-   * UnionField which stores the field name since the Field available in MyField stores the set value
-   * and corrresponding name of the set value is available through name.
-   *
-   */
-//  public static class UnionField extends MyField {
-//    TFieldIdEnum tField;
-//    Field tagField;
-//
-//    protected UnionField() {
-//      super();
-//    }
-//
-//    public UnionField(TFieldIdEnum fieldMeta, Field value, Field tag, ObjectInspector fieldObjectInspector) {
-//      super(value, fieldObjectInspector);
-//      this.tField = fieldMeta;
-//      this.tagField = tag;
-//    }
-//
-//    public String getFieldName() {
-//      return tField.getFieldName().toLowerCase();
-//    }
-//
-//    public ObjectInspector getFieldObjectInspector() {
-//      return fieldObjectInspector;
-//    }
-//
-//    public String getFieldComment() {
-//      return null;
-//    }
-//
-//    @Override
-//    public String toString() {
-//      return field.toString();
-//    }
-//  }
-
-  /**
    * This method is only intended to be used by Utilities class in this package.
    * The reason that this method is not recursive by itself is because we want
    * to allow recursive types.
@@ -115,7 +76,6 @@ public class ThriftUnionObjectInspector extends ReflectionStructObjectInspector 
                       ObjectInspectorFactory.ObjectInspectorOptions options) {
      this.objectClass = objectClass;
      final Field fieldMetaData;
-//     final Field fieldList;
 
      try {
         fieldMetaData = objectClass.getDeclaredField(FIELD_METADATA_MAP);
@@ -126,25 +86,17 @@ public class ThriftUnionObjectInspector extends ReflectionStructObjectInspector 
       }
 
       this.fields = new ArrayList<StructField>();
-//      int used = 0;
       try {
         final Map<? extends TFieldIdEnum, FieldMetaData> fieldMap = (Map<? extends TFieldIdEnum, FieldMetaData>) fieldMetaData.get(null);
-//        Field unionValueField = objectClass.getSuperclass().getDeclaredField(UNION_VALUE_FIELD);
-//        Field unionTagField = objectClass.getSuperclass().getDeclaredField(UNION_TAG_FIELD);
-//        unionValueField.setAccessible(true);
         this.ois = new ArrayList<ObjectInspector>();
         for(Map.Entry<? extends TFieldIdEnum, FieldMetaData> metadata : fieldMap.entrySet()) {
           final Type fieldType = ThriftObjectInspectorUtils.getFieldType(objectClass, metadata.getValue().fieldName);
           final ObjectInspector reflectionObjectInspector = ObjectInspectorFactory.getReflectionObjectInspector(fieldType, options);
-//          this.fields.add(new UnionField(metadata.getKey(), unionValueField, unionTagField, reflectionObjectInspector));
           this.ois.add(reflectionObjectInspector);
         }
       } catch (IllegalAccessException e) {
         throw new RuntimeException("Unable to find field metadata for thrift union field ", e);
       }
-//      } catch (NoSuchFieldException e) {
-//        throw new RuntimeException("Unable to find field from thrift generated code ", e);
-//      }
   }
 
   @Override
@@ -160,7 +112,6 @@ public class ThriftUnionObjectInspector extends ReflectionStructObjectInspector 
   public String getTypeName() {
     return ObjectInspectorUtils.getStandardUnionTypeName(this);
   }
-
 
   @Override
   public Object create() {
